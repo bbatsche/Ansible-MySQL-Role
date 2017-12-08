168f90bca1555091bc36fc9f7685955f2db7321c
Ben Batschelet <bbatschelet@socialsolutions.com>
2017-12-08 17:01:09 -0600

    Add install and user management options; still need to update config and specs; lots of commented work currently

---
+++ spec/install-mysql-spec.rb
@@ -0,0 +1,44 @@
+require_relative "bootstrap"
+
+RSpec.configure do |config|
+  config.before :suite do
+    AnsibleHelper.playbook("playbooks/install.yml", ENV["TARGET_HOST"], {install_mysql: true})
+  end
+end
+#
+# describe command("mysql --version") do
+#   its(:stdout) { should match /\b5\.5\.\d+/ }
+#
+#   its(:exit_status) { should eq 0 }
+# end
+#
+# describe command("mysqld --version") do
+#   its(:stdout) { should match /\b5\.5\.\d+/ }
+#
+#   its(:exit_status) { should eq 0 }
+# end
+
+describe service('mysql') do
+  let(:disable_sudo) { false }
+  it { should be_running }
+end
+
+describe command("mysql -u vagrant -pvagrant -e \"SELECT 'MySQL Installed'\"") do
+  its(:stdout) { should match /MySQL Installed/ }
+
+  its(:exit_status) { should eq 0}
+end
+
+describe command("mysql -u root") do
+  its(:stderr) { should match /Access denied for user 'root'@'localhost'/ }
+
+  its(:exit_status) { should_not eq 0}
+end
+#
+# describe command("mysql -u vagrant -pvagrant -e \"SHOW VARIABLES LIKE 'slow_query_log'\"") do
+#   its(:stdout) { should match /ON/ }
+# end
+#
+# describe command("mysql -u vagrant -pvagrant -e \"SHOW VARIABLES LIKE 'log_queries_not_using_indexes'\"") do
+#   its(:stdout) { should match /ON/ }
+# end
--- spec/mysql-not-dev-spec.rb
+++ spec/mysql-not-dev-spec.rb
@@ -2,14 +2,17 @@ require_relative "bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
-    AnsibleHelper.playbook("playbooks/mysql-install.yml", ENV["TARGET_HOST"], env_name: "production")
+    AnsibleHelper.playbook("playbooks/install.yml", ENV["TARGET_HOST"], {
+      env_name: "production",
+      install_mysql: true
+    })
   end
 end
 
-describe command("mysql -u vagrant -pvagrant -e \"SHOW VARIABLES LIKE 'slow_query_log'\"") do
-  its(:stdout) { should match /OFF/ }
-end
-
-describe command("mysql -u vagrant -pvagrant -e \"SHOW VARIABLES LIKE 'log_queries_not_using_indexes'\"") do
-  its(:stdout) { should match /OFF/ }
-end
+# describe command("mysql -u vagrant -pvagrant -e \"SHOW VARIABLES LIKE 'slow_query_log'\"") do
+#   its(:stdout) { should match /OFF/ }
+# end
+#
+# describe command("mysql -u vagrant -pvagrant -e \"SHOW VARIABLES LIKE 'log_queries_not_using_indexes'\"") do
+#   its(:stdout) { should match /OFF/ }
+# end
