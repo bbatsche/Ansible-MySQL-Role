6367e71b7f19159d992b6653b77048133045fa2a
Ben Batschelet <bartleby84@gmail.com>
2018-11-17 22:32:30 -0600

    Drop support for Trusty Updates for Ansible 2.7 Delete our fork of mysql_user module

--- spec/lib/environments.rb
+++ spec/lib/environments.rb
@@ -5,7 +5,6 @@ require_relative "vagrant_env"
 if ENV["CONTINUOUS_INTEGRATION"] == "true"
   # CI server will use docker for environments
   [
-    {:name => "trusty", :image => Docker::Image.create("fromImage" => "ubuntu:trusty")},
     {:name => "xenial", :image => Docker::Image.create("fromImage" => "ubuntu:xenial")},
     {:name => "bionic", :image => Docker::Image.build("from ubuntu:bionic\nrun apt-get update && apt-get install -y init")}
   ].each do |vm|
@@ -14,7 +13,7 @@ if ENV["CONTINUOUS_INTEGRATION"] == "true"
     AnsibleHelper << env
   end
 else
-  ["trusty", "xenial", "bionic"].each do |vm|
+  ["xenial", "bionic"].each do |vm|
     env = VagrantEnv.new(vm)
     env.use_python3 = ENV["ANSIBLE_PYTHON_VERSION"] == "3"
     AnsibleHelper << env
