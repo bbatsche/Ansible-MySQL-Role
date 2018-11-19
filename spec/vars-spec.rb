34e6c9c9a0a0c8c354c0940f516980a6a7566821
Ben Batschelet <bartleby84@gmail.com>
2018-11-19 15:19:57 -0600

    Fix memory based specs & auth socket

--- spec/vars-spec.rb
+++ spec/vars-spec.rb
@@ -18,36 +18,36 @@ end
 context "MySQL variables" do
   let(:mysql_password) { "vagrant" }
   let(:totalMem) { host_inventory["memory"]["total"].to_i }
-  
+
   describe "InnoDB buffer pool size" do
     let(:subject) { command(%Q{mysql -Nqs -uvagrant -pvagrant -e "SHOW VARIABLES LIKE 'innodb_buffer_pool_size'"}) }
-    
+
     it "is set based on total memory" do
-      expect(subject.stdout.gsub(/innodb_buffer_pool_size\s+(.+)$/, '\1').to_i / 1024).to be_within(2048).of((totalMem * 0.8 * 0.6).to_i)
+      expect(subject.stdout.gsub(/innodb_buffer_pool_size\s+(.+)$/, '\1').to_i / 1024).to be_within(5120).of((totalMem * 0.8 * 0.6).to_i)
     end
   end
-  
+
   describe "Key buffer size" do
     let(:subject) { command(%Q{mysql -Nqs -uvagrant -pvagrant -e "SHOW VARIABLES LIKE 'key_buffer_size'"}) }
-    
+
     it "is set based on total memory" do
-      expect(subject.stdout.gsub(/key_buffer_size\s+(.+)$/, '\1').to_i / 1024).to be_within(2048).of((totalMem * 0.8 * 0.2).to_i)
+      expect(subject.stdout.gsub(/key_buffer_size\s+(.+)$/, '\1').to_i / 1024).to be_within(5120).of((totalMem * 0.8 * 0.2).to_i)
     end
   end
-  
+
   describe "SQL mode" do
     let(:subject) { command(%Q{mysql -Nqs -uvagrant -pvagrant -e "SHOW VARIABLES LIKE 'sql_mode'"}) }
-    
+
     it "is set in config" do
       expect(subject.stdout.gsub(/sql_mode\s+(.+)$/, '\1')).not_to match /REAL_AS_FLOAT/
       expect(subject.stdout.gsub(/sql_mode\s+(.+)$/, '\1')).not_to match /PIPES_AS_CONCAT/
       expect(subject.stdout.gsub(/sql_mode\s+(.+)$/, '\1')).not_to match /ANSI_QUOTES/
-      
+
       expect(subject.stdout.gsub(/sql_mode\s+(.+)$/, '\1')).to match /STRICT_TRANS_TABLES/
       expect(subject.stdout.gsub(/sql_mode\s+(.+)$/, '\1')).to match /STRICT_ALL_TABLES/
     end
   end
-  
+
   describe "bind address" do
     include_examples("mysql variable", "bind_address", "*")
   end
